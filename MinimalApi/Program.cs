8f5a4a7db03131856239d7b97a863efd9854e262
robsonlinhares <8KWPDuVTe3RSPxB>
2022-01-27 21:17:23 -0300

    add swagger, get todo, post todo

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -1,6 +1,33 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using MinimalApi.Data;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapGet("v1/todos", (AppDbContext context) =>
+{
+    var todos = context.Todos.ToList();
+    return Results.Ok(todos);
+}).Produces<Todo>();
+
+app.MapPost("v1/todos", (
+    AppDbContext context,
+    CreateTodoViewModel model) =>
+{
+    var todo = model.MapTo();
+    if (!model.IsValid)
+        return Results.BadRequest(model.Notifications);
+
+    context.Todos.Add(todo);
+    context.SaveChanges();
+
+    return Results.Created($"/v1/todos/{todo.Id}", todo);
+
+});
 
 app.Run();
---
+++ MinimalApi/ViewModels/CreateTodoViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+
+public class CreateTodoViewModel : Notifiable<Notification>
+{
+    public string Title { get; set; }
+
+    public Todo MapTo()
+    {
+        var contract = new Contract<Notification>()
+            .Requires()
+            .IsNotNull(Title, "Informe o título da tarefa")
+            .IsGreaterThan(Title, 5, "O título deve conter mais que 5 caracteres");
+
+        AddNotifications(contract);
+
+        return new Todo(Guid.NewGuid(), Title, false);
+    }
+}
+
